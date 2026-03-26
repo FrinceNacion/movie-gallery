3c169c93547a6fbfaea664aa34c2fa5a47ee020b
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-03-26 22:18:15 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Movie Gallery</h1>
+        <p>Welcome to the Movie Gallery!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
