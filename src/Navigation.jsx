cb6116808d4e6c587042dfe65ca53723da246c8d
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-03-26 23:57:56 +0800

    Update: initial nav style

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,12 +1,12 @@
 function Navigation() {
     return (
         <>
-            <nav>
-                <h4>Movie Gallery</h4>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/movies">Movies</a></li>
-                    <li><a href="/about">About</a></li>
+            <nav className="d-flex flex-row justify-content-between align-items-center p-3">
+                <h3>Movie Gallery</h3>
+                <ul className="d-flex flex-row justify-content-around align-items-center w-25"> 
+                    <li><p>Home</p></li>
+                    <li><p>History</p></li>
+                    <li><p>Search</p></li>
                 </ul>
             </nav>
         </>
