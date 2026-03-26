0133a3b124f328d0a089baef3ae1d7a8410cb2aa
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-03-26 22:44:21 +0800

    Add: Navigation.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+import { useState } from 'react'
+import './Navigation.jsx'
+import Navigation from './Navigation.jsx'
 
 function App() {
 
   return (
     <>
       <div>
+        <Navigation />
         <h1>Movie Gallery</h1>
         <p>Welcome to the Movie Gallery!</p>
       </div>
---
+++ src/Navigation.jsx
@@ -0,0 +1,16 @@
+function Navigation() {
+    return (
+        <>
+            <nav>
+                <h4>Movie Gallery</h4>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/movies">Movies</a></li>
+                    <li><a href="/about">About</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Navigation;
