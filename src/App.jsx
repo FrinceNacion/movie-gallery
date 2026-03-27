fa1b00245b5b916f6d6a9c31a4bb68f14f087304
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-03-27 20:08:52 +0800

    Update: minor style change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './Navigation.jsx'
 import Navigation from './Navigation.jsx'
+import MovieGrid from './MovieGrid.jsx'
 
 function App() {
 
   return (
     <>
-      <div>
+      <div className="container d-flex flex-column gap-2">
         <Navigation />
         <h1>Movie Gallery</h1>
         <p>Welcome to the Movie Gallery!</p>
+        <MovieGrid />
       </div>
     </>
   )
