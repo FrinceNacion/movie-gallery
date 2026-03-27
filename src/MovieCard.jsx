f0c9bb5d407653851216dbec6f5c7d5a568c2d8b
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-03-27 20:09:55 +0800

    Add: MovieCard.jsx

---
+++ src/MovieCard.jsx
@@ -0,0 +1,15 @@
+
+function MovieCard(movie) {
+    return (
+        <div className="">
+            { movie ? (
+                <a href="">
+                    <img className="img-fluid rounded-2" src={movie.poster} alt={movie.title}/>
+                </a>
+            ) : (<p>Loading</p>)}
+        </div>
+    );
+}
+
+
+export default MovieCard;
