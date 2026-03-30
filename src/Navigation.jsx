2167dd87c00c4ddbbc683b262dfb1c0f1fd5c72a
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-03-30 21:32:55 +0800

    Update: added navbar class

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,7 +1,7 @@
 function Navigation() {
     return (
         <>
-            <nav className="d-flex flex-row justify-content-between align-items-center p-3">
+            <nav className="navbar d-flex flex-row justify-content-between align-items-center p-3">
                 <h3>Movie Gallery</h3>
                 <div className="d-flex flex-column justify-content-between d-md-none">
                     <button className="btn btn-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
