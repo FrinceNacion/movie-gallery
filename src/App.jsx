f094b742228ddf5e9c6957f74fb1d10705d07e05
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-03-31 19:45:20 +0800

    Update: implemented initial Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './Navigation.jsx'
 import Navigation from './Navigation.jsx'
 import MovieGrid from './MovieGrid.jsx'
+import Footer from './Footer.jsx'
 
 function App() {
 
   return (
     <>
-      <div className="container-fluid d-flex flex-column gap-2 h-100">
+      <div className="container-fluid d-flex flex-column gap-2 h-100 p-0">
         <Navigation />
         <MovieGrid />
+        <Footer />
       </div>
     </>
   )
