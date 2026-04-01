ed0965644f794b59016814ec0cd11cea726adab3
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-04-01 22:25:19 +0800

    Update: enhanced layout and content

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,13 +2,23 @@ import "./index.css"
 
 function Footer(){
     return (
-        <footer className="footer d-flex flex-row justify-content-between p-3">
-            <div className="d-flex flex-column w-50 justify-content-between g-2">
-                <p>This project is for education purposes only. It aggregates streaming links from various sites already available on the internet but does not host or provide the actual video content.</p>
-                <p>Created with love by frincefriess 🍟</p>
+        <footer className="footer d-flex flex-row justify-content-evenly p-3 flex-wrap">
+            <div className="d-flex flex-column w-25 g-3">
+                <p>Note:</p>
+                <p>This project is for education purposes only. <span className="d-none d-sm-block">It aggregates streaming links from various sites already available on the internet but does not host or provide the actual video content.</span> </p>
             </div>
-            <div className="d-flex flex-column justify-content-center g-2">
-                <p>Github: <a style={{ textDecoration: 'none' }} href="https://github.com/FrinceNacion" target="_blank" rel="noopener noreferrer">FrinceNacion</a></p>
+            <div className="d-flex flex-column g-3 d-none d-sm-flex">
+                <p>Tools:</p>
+                <ul>
+                    <li>React</li>
+                    <li>PHP</li>
+                    <li>Bootstrap</li>
+                </ul>
+            </div>
+            <div className="d-flex flex-column g-3 w-30">
+                <p>Me: </p>
+                <a style={{ textDecoration: 'none' }} href="https://github.com/FrinceNacion" target="_blank" rel="noopener noreferrer">Github</a>
+                <p>Created with love by frincefriess 🍟</p>
             </div>
         </footer>
     )
