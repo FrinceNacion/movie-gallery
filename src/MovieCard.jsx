4b52780e66864e8da08d2884c03bdd8c9fc66701
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-04-03 15:03:03 +0800

    Update: initial info-pill

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -5,9 +5,15 @@ function MovieCard({movie}) {
         <div className="movie-card">
             { movie ? (
                 <a href="">
-                    <img className="img-fluid rounded-2" src={movie.poster} alt={movie.title}/>
+                    <img className="img-fluid rounded-top-2" src={movie.poster} alt={movie.title}/>
                 </a>
             ) : (<p>Loading</p>)}
+            <div className="info-pill d-flex flex-row rounded-bottom-2 w-100 p-1">
+                <div className="d-flex flex-row justify-content-around w-100">
+                    <p className="p-subtle">save</p>
+                    <p className="p-subtle">similar</p>
+                </div>
+            </div>
         </div>
     );
 }
